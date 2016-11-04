e8839f85b9b95d8827a5b540db6b51766cd3df05
wenidngding <wendingding@qq.com>
2016-11-04 11:36:21 +0800

    更新仓库

---
+++ Python/01-简单认识.h
@@ -0,0 +1,27 @@
+01-简单认识
+   ① python翻译为蟒蛇
+   ② python也可以帮助我们完成大部分的编程工作
+   ③ 特点：跨平台，可以在多种操作系统中运行
+   ④ 优势：更轻量级(用更少的代码完成工作)
+   ⑤ 应用：
+        🌂 mac系统自带Python的解释环境
+        🌂 豆瓣纯粹使用python开发
+        🌂 玛雅等3D动画工具嵌入了python代码
+        🌂 企业应用和云计算等
+    ⑥ 学习阶段：常用语法|面向对象编程|模块编程|游戏编|计算机仿真
+
+
+02-处理python的环境
+    ① 环境
+        🌂 检查系统的Python环境
+            打开终端，输入Python -V查看系统自带的版本
+    ② 下载
+        🌂 去官网下载安装新版本的Python
+            官网地址：https://www.python.org/downloads/
+    ③ 简单使用
+        输出字符串：  print("hello world")
+        输出计算结果: print(3+5)
+        字符串拼接：  "hello" + "world"
+        重复字符串    print("wendingding" * 3)
+        
+
